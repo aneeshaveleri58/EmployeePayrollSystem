57aa85a9aef6fae6bf1f32b8183eafd9874a3ff6
Aneesha <aneeshaveleri58@gmail.com>
2025-02-05 05:27:27 -0500

    DisplayDetails And CalculateSalary Functions Added In BaseEmployee

--- EmployeePayrollSystem/BaseEmployee.cs
+++ EmployeePayrollSystem/BaseEmployee.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace EmployeePayrollSystem
+public class BaseEmployee
 {
-    internal class BaseEmployee
+    public string Name { get; set; }
+    public int ID { get; set; }
+    public string Role { get; set; }
+    public decimal BasicPay { get; set; }
+    public decimal Allowances { get; set; }
+
+    public BaseEmployee(string name, int id, string role, decimal basicPay, decimal allowances)
     {
+        Name = name;
+        ID = id;
+        Role = role;
+        BasicPay = basicPay;
+        Allowances = allowances;
+    }
+
+    public virtual decimal CalculateSalary()
+    {
+        decimal deductions = 0.1m * BasicPay; // Example deduction as 10% of basic pay
+        return BasicPay + Allowances - deductions;
+    }
+
+    public virtual void DisplayDetails()
+    {
+        Console.WriteLine($"ID: {ID}, Name: {Name}, Role: {Role}, Basic Pay: {BasicPay}, Allowances: {Allowances}");
     }
 }
