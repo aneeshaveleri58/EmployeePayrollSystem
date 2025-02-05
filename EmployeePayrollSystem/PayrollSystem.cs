99689ed28a27993234ec02e4b2a7d1775146e401
Aneesha <aneeshaveleri58@gmail.com>
2025-02-05 05:06:13 -0500

    UserInterface Modified

--- EmployeePayrollSystem/PayrollSystem.cs
+++ EmployeePayrollSystem/PayrollSystem.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EmployeePayrollSystem;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace EmployeePayrollSystem
+//namespace EmployeePayrollSystem
+//{
+//    internal class PayrollSystem
+//    {
+
+
+//    }
+//}
+using System;
+using System.Collections.Generic;
+
+public class PayrollSystem
 {
-    internal class PayrollSystem
+    private List<BaseEmployee> employees = new List<BaseEmployee>();
+
+    public void AddEmployee()
     {
+       
+    }
+
+    public void DisplayAllEmployees()
+    {
+      
+    }
+
+    public void DisplayEmployeeSalary()
+    {
+      
+    }
+
+    public void DisplayTotalPayroll()
+    {
+       
     }
 }
--- EmployeePayrollSystem/Program.cs
+++ EmployeePayrollSystem/Program.cs
@@ -1,15 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
-namespace EmployeePayrollSystem
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        PayrollSystem payrollSystem = new PayrollSystem();
+        bool exit = false;
+
+        while (!exit)
         {
+            Console.Clear();
+            Console.WriteLine("Employee Payroll System");
+            Console.WriteLine("1. Add Employee");
+            Console.WriteLine("2. Display All Employee Details");
+            Console.WriteLine("3. Display Employee Salary");
+            Console.WriteLine("4. Display Total Payroll");
+            Console.WriteLine("5. Exit");
+            Console.Write("Enter your choice: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    payrollSystem.AddEmployee();
+                    break;
+                case 2:
+                    payrollSystem.DisplayAllEmployees();
+                    break;
+                case 3:
+                    payrollSystem.DisplayEmployeeSalary();
+                    break;
+                case 4:
+                    payrollSystem.DisplayTotalPayroll();
+                    break;
+                case 5:
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice, please try again.");
+                    break;
+            }
+
+            if (!exit)
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
         }
     }
 }
